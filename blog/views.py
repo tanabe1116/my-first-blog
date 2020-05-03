f6200852909af84ab3af3ebd54be87b39d0a4e20
tanaka <ymbs_pg_11_tcl@yahoo.co.jp>
2020-05-03 18:28:56 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -9,4 +9,4 @@ def post_list(request):
     }
     posts = Post.order_by('published_date')
     # https://qiita.com/shonansurvivors/items/12b087cf5ab591273c8c
-    return render(request, 'blog/post_list.html', d)
+    return render(request, './blog/post_list.html', d)
