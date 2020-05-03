da9ac8d3891be2d98f159f267c93e952062d94d0
tanaka <ymbs_pg_11_tcl@yahoo.co.jp>
2020-05-03 18:27:13 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     d = {
-        objects': range(10),
+        'objects': range(10),
     }
     posts = Post.order_by('published_date')
     # https://qiita.com/shonansurvivors/items/12b087cf5ab591273c8c
