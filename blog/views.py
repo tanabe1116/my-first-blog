53d034dd8e996bfb0f008be913251cbc172ac73d
tanaka <ymbs_pg_11_tcl@yahoo.co.jp>
2020-05-03 18:26:32 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,9 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
+    d = {
+        objects': range(10),
+    }
     posts = Post.order_by('published_date')
     # https://qiita.com/shonansurvivors/items/12b087cf5ab591273c8c
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', d)
