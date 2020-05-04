575d015181c9e295f0b85a448a144d958b5ec41c
tanaka <ymbs_pg_11_tcl@yahoo.co.jp>
2020-05-04 13:06:59 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -6,13 +6,13 @@ from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
-    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     # https://qiita.com/shonansurvivors/items/12b087cf5ab591273c8c
-    return render(request, 'blog/post_list.html', {'posts': Post.objects.all()})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html')
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
     if request.method == "POST":
